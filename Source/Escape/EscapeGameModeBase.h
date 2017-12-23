283f08ffc9707931a001892f2ffa2f6134051340
Nico Miebach <nico@miebach.one>
2017-12-23 18:35:32 +0100

    Initial Commit

---
+++ Source/Escape/EscapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_API AEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
